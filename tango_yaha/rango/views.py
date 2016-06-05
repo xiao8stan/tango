e3561424cb20b094646b4cec44308e9ab5e0f549
stan_yaha <175501069@qq.com>
2016-06-05 14:55:23 +0800

    finish the bootstarp chapter

--- tango_yaha/rango/views.py
+++ tango_yaha/rango/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from rango.models import Category, Page
 from rango.forms import CategoryForm
 from rango.forms import PageForm
-from rango.forms import UserForm, UserProfileForm
+from django.contrib.auth.decorators import login_required
+from datetime import datetime
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse, HttpResponseRedirect
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout
-from datetime import datetime
+# from django.contrib.auth import logout
+# from rango.forms import UserForm, UserProfileForm
+
 
 
 def index(request):
@@ -108,37 +109,37 @@ def add_page(request, category_name_slug):
     return render(request, 'rango/add_page.html', context_dict)
 
 
-def register(request):
-
-    registered = False
-    if request.method == 'POST':
-        user_form = UserForm(data=request.POST)
-        profile_form = UserProfileForm(data=request.POST)
-
-        if user_form.is_valid() and profile_form.is_valid():
-            user = user_form.save()
-            user.set_password(user.password)
-            user.save()
-
-            profile = profile_form.save(commit=False)
-            profile.user = user
-
-            if 'picture' in request.FILES:
-                profile.picture = request.FILES['pictures']
-
-            profile.save()
-
-            registered = True
-
-        else:
-            print user_form.errors, profile_form.errors
-
-    else:
-        user_form = UserForm()
-        profile_form = UserProfileForm()
-
-    return render(request, 'rango/register.html',
-                  {'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
+# def register(request):
+#
+#     registered = False
+#     if request.method == 'POST':
+#         user_form = UserForm(data=request.POST)
+#         profile_form = UserProfileForm(data=request.POST)
+#
+#         if user_form.is_valid() and profile_form.is_valid():
+#             user = user_form.save()
+#             user.set_password(user.password)
+#             user.save()
+#
+#             profile = profile_form.save(commit=False)
+#             profile.user = user
+#
+#             if 'picture' in request.FILES:
+#                 profile.picture = request.FILES['pictures']
+#
+#             profile.save()
+#
+#             registered = True
+#
+#         else:
+#             print user_form.errors, profile_form.errors
+#
+#     else:
+#         user_form = UserForm()
+#         profile_form = UserProfileForm()
+#
+#     return render(request, 'rango/register.html',
+#                   {'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
 
 
 def user_login(request):
@@ -167,9 +168,9 @@ def user_login(request):
 def restricted(request):
     return render(request, 'rango/restricted.html', {})
 
-
-@login_required
-def user_logout(request):
-    logout(request)
-
-    return HttpResponseRedirect('/rango')
+#
+# @login_required
+# def user_logout(request):
+#     logout(request)
+#
+#     return HttpResponseRedirect('/rango')
--- tango_yaha/tango_yaha/settings.py
+++ tango_yaha/tango_yaha/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -53,13 +54,19 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
+REGISTRATION_OPEN = True
+ACCOUNT_ACTIVATION_DAYS = 7
+REGISTRATION_AUTO_LOGIN = True
+LOGIN_REDIRECT_URL = '/rango/'
+LOGIN_URL = '/accounts/login/'
+
+
 ROOT_URLCONF = 'tango_yaha.urls'
 
 WSGI_APPLICATION = 'tango_yaha.wsgi.application'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-LOGIN_URL = '/rango/login/'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
